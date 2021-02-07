99337b303174ee5573ca722c014e6022329f26f9
VISWESHKANNAN <77962314+vichu2010@users.noreply.github.com>
2021-02-07 14:49:11 +0530

    Update Ebot.py

--- Ebot.py
+++ Ebot.py
@@ -1,5 +1,5 @@
 import smtplib
 Server=smtplib.SMTP('smtp.gmail.com',587)
 Server.starttls()
-Server.login('visweshkannan2000@gmail.com','@0502162010')
+Server.login('mailid','password')
 Server.sendmail('visweshkannan2000@gmail','mec.it.visweshkannan@gmail.com','Hello today is the last date for submitting NPTEL assignment-2')
